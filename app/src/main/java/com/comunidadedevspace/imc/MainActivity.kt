307ca48cbf1815a9c505ee87f4fd3e93ca6b372b
IgorCorse <igor.f.corse@gmail.com>
2024-09-10 14:46:21 +0100

    Mostra mensagem "preencha todos os campos"

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -10,19 +11,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+
         val edtpeso = findViewById<TextInputEditText>(R.id.edit_peso)
         val edtaltura = findViewById<TextInputEditText>(R.id.edit_altura)
+
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
         btnCalcular.setOnClickListener {
-            val peso: Float = edtpeso.text.toString().toFloat()
-            val altura: Float = edtaltura.text. toString().toFloat()
 
-            val alturaQ2 = altura*altura
+
+
+
+            val pesoStr: String = edtpeso.text.toString()
+            val alturaStr: String = edtaltura.text. toString()
+
+            if(pesoStr =="" || alturaStr ==""){
+                //mostrar mensagem user
+
+                Snackbar.make(
+                    edtpeso,
+                    "Preencha todos os campos.",
+                    Snackbar.LENGTH_LONG
+                )
+                    .show()
+
+            } else{
+            val peso = pesoStr.toFloat()
+            val altura = alturaStr.toFloat()
+
+            val alturaQ2 = altura * altura
             val resultado = peso/alturaQ2
 
+            println("Igor - " + resultado)
 
-            println("Igor" + resultado)
+
+
+
+
+
+        }
 
         }
 
